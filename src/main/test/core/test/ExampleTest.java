f4f7f73944748b4c54702940697206f0061c6680
Melissa Sheehan <mxsheehan7@gmail.com>
2017-11-15 23:32:45 -0800

    all 3 test files

--- src/main/test/core/test/ExampleTest.java
+++ src/main/test/core/test/ExampleTest.java
@@ -18,16 +18,37 @@ public class ExampleTest {
     public void setup() {
         this.admin = new Admin();
     }
+    //Definitinons for this given test suite, this allows u to define now so u don't have to keep re-defining for each test
+    //Test suite:the context/environment 
 
+    
+    
     @Test
     public void testMakeClass() {
         this.admin.createClass("Test", 2017, "Instructor", 15);
         assertTrue(this.admin.classExists("Test", 2017));
     }
+    //passes this test case
 
     @Test
     public void testMakeClass2() {
         this.admin.createClass("Test", 2016, "Instructor", 15);
         assertFalse(this.admin.classExists("Test", 2016));
     }
-}
+    //does not pass this test case (assertion failure - expected false and got true)
+    //incorrectly accepts a class made in 2016 (incorrect implementation)
+    
+//    @Test
+//    public void testName() {
+//    	
+//    }
+    
+    
+    
+  //capacity: check corner cases ( 0, 1, -1), correct cases, and incorrect cases 
+    
+    }
+    
+
+
+ 
